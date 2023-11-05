88e6c9021cc80ad0cbe81bfe1161d79c472c596d
Nightfeller <family2earls@gmail.com>
2023-11-04 19:14:41 -0500

    fixed small bugs and ran the seeds into the database

--- models/index.js
+++ models/index.js
@@ -9,9 +9,9 @@ Product.belongsTo(Category);
 // Categories have many Products
 Category.hasMany(Product);
 // Products belongToMany Tags (through ProductTag)
-Product.belongToMany(Tag, { through: 'ProductTag' });
+Product.belongsToMany(Tag, { through: 'ProductTag' });
 // Tags belongToMany Products (through ProductTag)
-Tag.belongToMany(Product, { through: 'ProductTag' });
+Tag.belongsToMany(Product, { through: 'ProductTag' });
 
 module.exports = {
   Product,
